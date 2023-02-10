6078c82ff1f65ff1e613201802b0f307c32d4a92
Demira4 <97722493+Demira4@users.noreply.github.com>
2023-02-10 14:25:05 +0300

    HomeWork 7
    add in pom.xml dependency and plugin for allure

---
+++ ArticleWriteTest.java
@@ -0,0 +1,31 @@
+package Lesson6;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.util.Collections;
+import java.util.List;
+
+public class ArticleWriteTest extends AbstractTest {
+
+    @Test
+    void ArticleWriteTest(){
+        new LJMainPage(getWebDriver()).goToAccountPage();
+        new WebDriverWait(getWebDriver(), Duration.ofSeconds(5));
+        // Assertions.assertTrue(getWebDriver().getTitle().equals("Главное - ЖЖ"), "страница входа недоступна");
+        new LoginPage(getWebDriver())
+                .loginIn("Iren234","Q5ZFNdGLigxqZxZ11");
+        new WriteArticle(getWebDriver()).clickButtonNapisatStatiu();
+        Assertions.assertTrue(getWebDriver().getCurrentUrl().contains("post"), "Ошибка при нажатии кнопки Написать статью");
+        new WebDriverWait(getWebDriver(), Duration.ofSeconds(5));
+
+        //new WriteArticle(getWebDriver()).clickNoChernovik();
+
+        new WriteArticle(getWebDriver()).wrAr("Thanks", "Выражаю особую благодарность журналу ЖЖ в практике тестирования Веб-сайтов");
+        new LJMainPage(getWebDriver()).goOutAccountPage();
+
+    }
+}
